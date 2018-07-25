fbef6617ec34f985bb68a03e92f8afa84210d62a
Jonathan Tung <jtung23@gmail.com>
2018-07-24 19:18:51 -0700

    set up inquirer and changed linux script executable to react-component

--- create-react.js
+++ create-react.js
@@ -1,34 +1,60 @@
 const fs = require('fs')
-const method = process.argv[2]
-const name = process.argv[3]
-if (method !== "class" && method !== "component") {
-    console.log(method)
-    console.log('Format: createReact class || component name')
-    return
-}
+const inquirer = require('inquirer')
 
-const classText = `import React, {Component} from 'react'
+inquirer.prompt([ 
+    { // prompts with list asking for class or functional (or cancel)
+        type: "list",
+        name: "method",
+        message: "What type of component?",
+        choices: ['class', 'functional', 'cancel']
+    },
+    { // input name of component
+        type: "input",
+        name: "name",
+        message: "Name of component?"
+    }
+]).then(res => {
+    if (res.name === '' || res.method === 'cancel') { // option to cancel component creation
+        console.log('cancelled')
+        return
+    }
 
-class ${name} extends Component {
+    // class component template
+    const classText = `import React, {Component} from 'react'
+
+class ${res.name} extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
         return (
+            <div>
+            
+            </div>
         )    
     }
 }
-export default ${name}`
+export default ${res.name}`
 
-const componentText = `import React from 'react'
+    // functional component template
+    const functionalText = `import React from 'react'
 
-const ${name} = props => 
+const ${res.name} = props => 
     <div>
 
     </div>
 
-export default ${name}`
+export default ${res.name}`
 
-if (!fs.existsSync(name)) {
-    fs.mkdirSync(name)
-    fs.writeFileSync(`${name}/index.js`, `export {default} from './${name}'`)
-    fs.writeFileSync(`${name}/${name}.js`, method === "class" ? classText: componentText)
+    if (!fs.existsSync(res.name)) {
+        fs.mkdirSync(res.name) // creates folder
+        // creates index.js in folder with export default of component file
+        fs.writeFileSync(`${res.name}/index.js`, `export {default} from './${res.name}'`) 
+        // creates component file in folder where template is chosen based on the method chosen in inquirer prompt
+        fs.writeFileSync(`${res.name}/${res.name}.js`, res.method === "class" ? classText: functionalText)
 
-}
+    } 
+})
+
+
